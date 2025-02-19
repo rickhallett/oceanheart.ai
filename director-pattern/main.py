78adf7897ecfb7bb0483fa741643d163dac9a4b4
Rick Hallett <rickhallett@icloud.com>
2025-02-19 08:03:47 +0000

    chore: move paic starter and add anthropic cookbook and gemini structured extraction as submodules

---
+++ director-pattern/main.py
@@ -0,0 +1,14 @@
+from modules.data_types import MockDataType
+
+
+def main():
+
+    mock_data_type = MockDataType(id="1", name="idd")
+
+    print(mock_data_type)
+
+    print("Happy AI Coding 🚀")
+
+
+if __name__ == "__main__":
+    main()
