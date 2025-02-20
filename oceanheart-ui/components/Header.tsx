7a98805540cb8fa4417359bb3b9c294014254c18
Rick Hallett <rickhallett@icloud.com>
2025-02-20 16:59:22 +0000

    fix: console spam

--- oceanheart-ui/components/Header.tsx
+++ oceanheart-ui/components/Header.tsx
@@ -60,7 +60,6 @@ const Header = () => {
               src={logo}
               alt={`${config.appName} logo`}
               className="w-8"
-              placeholder="blur"
               priority={true}
               width={32}
               height={32}
@@ -127,7 +126,6 @@ const Header = () => {
                 src={logo}
                 alt={`${config.appName} logo`}
                 className="w-8"
-                placeholder="blur"
                 priority={true}
                 width={32}
                 height={32}
